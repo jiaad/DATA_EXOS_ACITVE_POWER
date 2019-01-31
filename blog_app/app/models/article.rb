41b535307959c773a7c57512a490a057df3eebc2
jiaad <abdulziad08@outlook.com>
2019-01-31 09:58:57 +0100

    oublié de push

--- blog_app/app/models/article.rb
+++ blog_app/app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
     belongs_to :user
+    has_many :category
+    has_many :tag
 end
--- blog_app/db/schema.rb
+++ blog_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_30_150459) do
+ActiveRecord::Schema.define(version: 2019_01_31_085602) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "author_id"
@@ -19,6 +19,18 @@ ActiveRecord::Schema.define(version: 2019_01_30_150459) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_articles_on_author_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.text "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
